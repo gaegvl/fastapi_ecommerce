8a8a865918b1073ebd353701b12082e0ea0d401e
egorgavrilov <Egor4it@yandex.ru>
2024-10-12 12:46:12 +0500

    Создание routers

---
+++ app/routers/products.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/products", tags=["products"])
+
+@router.get("/")
+async def all_products():
+    pass
+
+@router.post("/create")
+async def create_products():
+    pass
+
+@router.get("/{category_slug")
+async def get_products_by_category(category_slug: str):
+    pass
+
+@router.get("/detail/{product_slug}")
+async def product_detail(product_slug: str):
+    pass
+
+@router.put("/detail/{product_slug}")
+async def update_product(product_slug: str):
+    pass
+
+@router.delete("/detail/{product_slug}")
+async def delete_product(product_slug: str):
+    pass
