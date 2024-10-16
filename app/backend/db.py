26276a4ba504f4853c37216ce61b42a428b6f604
egorgavrilov <Egor4it@yandex.ru>
2024-10-16 22:51:43 +0500

    Переименование каталога

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,11 +1,14 @@
+import asyncpg
 from sqlalchemy.ext .asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import MetaData
+from sqlalchemy.dialects import registry
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-engine = create_async_engine(f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/postgres")
+engine = create_async_engine(f"postgresql+asyncpg://{os.getenv('DB_LOGIN')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/postgres?role=egor",
+                             )
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 metadata = MetaData(schema='fastapi_ecommerce')
 
