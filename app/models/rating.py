83cc20c411ff607df818bff8fccdd9048aad97e6
egorgavrilov <Egor4it@yandex.ru>
2024-10-22 23:09:12 +0500

    Создание модели rating

---
+++ app/models/rating.py
@@ -0,0 +1,16 @@
+from app.backend.db import Base
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+
+class Rating(Base):
+
+    __tablename__ = 'rating'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    grade: Mapped[int] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    product_id: Mapped[int] = mapped_column(ForeignKey('product.id'))
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    feedback: Mapped['Rating'] = relationship('Feedback', back_populates='rating', lazy='selectin')
