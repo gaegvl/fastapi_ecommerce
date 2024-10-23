c09885113d55abe581598bbaf56d7ee72dc7ea5a
egorgavrilov <Egor4it@yandex.ru>
2024-10-23 21:21:52 +0500

    Создание схем для feedbacks и rating

---
+++ app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .rating import CreateRating
+from .feedbacks import CreateFeedback
+from .users import CreateUser
+from .category import CreateCategory
+from .product import CreateProduct
--- app/schemas/feedbacks.py
+++ app/schemas/feedbacks.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
-
+from .rating import CreateRating
 
 class CreateFeedback(BaseModel):
     product_id: int
     comment: str
+    rating: CreateRating
